8fb539c561a68c444573b4ed750c40e001d16bec
AaronVelasco93 <aaronvelasco1933@outlook.com>
2024-02-21 12:31:24 -0600

    Clase 2102024

---
+++ 21202024/app1.js
@@ -0,0 +1,26 @@
+// Ingresando Datos
+// let apellido=prompt("Ingresa tu apellido");
+// console.log(apellido);
+/*
+let numeroUno=prompt("Ingresa el numero 1:");
+
+let numeroDos=prompt("Ingresa el numero 2:");
+
+let numero=30;
+console.log(typeof numero);
+
+let resultado=numeroUno+numeroDos;
+console.log(typeof numeroDos);
+console.log(resultado);
+
+*/
+
+let numeroUno=prompt("Ingresa el numero 1:");
+
+let numeroDos=prompt("Ingresa el numero 2:");
+
+let resultado= parseInt(numeroUno) + parseInt(numeroDos) ;
+console.log(typeof resultado);
+console.log(resultado);
+
+
